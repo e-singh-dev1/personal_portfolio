afe1e71023c9879cb4d61fc3bf5600bfa6a7d11d
Eshan Singh <eshansingh510@gmail.com>
2023-09-05 23:39:55 +0530

    update the file link as well.

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -35,7 +35,7 @@ const SocialLinks = () => {
 
             <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] 
              hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 rounded-br-md'>
-             <a href='/Eshan_Singh_Resume.pdf' className='flex justify-between items-center w-full text-white' download={true}>
+             <a href='/Eshan_CV.pdf' className='flex justify-between items-center w-full text-white' download={true}>
               <>
               Resume <BsFillPersonLinesFill size={30}/>
               </>
